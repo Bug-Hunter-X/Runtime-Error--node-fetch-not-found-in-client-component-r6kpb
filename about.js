f54739e57b02bb74b8dff2d5cd5264697393493e
BugHunter-X <askzenai@gmail.com>
2025-01-12 10:16:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,30 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import fetch from 'node-fetch'; //this is the problem
+
+export default async function About(){
+  const response = await fetch('https://api.example.com/data');
+  const data = await response.json();
+  return (
+    <div>
+      <h1>About Page</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+```
